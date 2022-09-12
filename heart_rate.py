5e83433025510642c65cb435c267a3e01c013a9a
Keaton Myers <keatonmyers01@gmail.com>
2022-09-12 14:07:14 -0400

    modified the data storage to work with json files and added comments

--- heart_rate.py
+++ heart_rate.py
@@ -1,22 +1,48 @@
 import time
+
+from numpy import record
 # Import SPI library (for hardware SPI) and MCP3008 library.
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_MCP3008
-from threading import Timer
-import meerschaum as mrsm
 import datetime
+import json
 
-def calculate_bpm(beats, trigger, pipe):
-    # Truncate beats queue to max, then calculate bpm.
-    # Calculate difference in time from the head to the
-    # tail of the list. Divide the number of beats by
-    # this duration (in seconds)
+
+# maintains one json file per day to avoid data bloating
+# if unable to sync to database
+def update_file_name():
+    global date
+    date = datetime.datetime.utcnow().strptime("%d:%m:%Y")
+    global Log_File_name 
+    Log_File_name = f".data_logs/temperature_data.json" + date
+    global file_data
+    file_data.clear()
+    file_data = {
+        "readings" : []
+    }
+
+# Truncate beats queue to max, then calculate bpm.
+# Calculate difference in time from the head to the
+# tail of the list. Divide the number of beats by
+# this duration (in seconds)
+def calculate_bpm(beats):
+    # if it moves to the next day while the script is running
+    record_time = datetime.datetime.utcnow()
+    if record_time.strptime("%d:%m:%Y") != date:
+        update_file_name()
     beats = beats[-TOTAL_BEATS:]
     beat_time = beats[-1] - beats[0]
-    if beat_time and trigger:
-        bpm = (len(beats) / (beat_time)) * 60
-    #    print(f"bpm: {bpm}")
-        pipe.sync({"UTCDateTime":[datetime.datetime.utcnow()],"bpm":[bpm]})
+    
+    bpm = (len(beats) / (beat_time)) * 60
+    global file_data
+    file_data["readings"].append(
+        {
+            "UTCDatetime": record_time,
+            "bmp": bpm
+        }
+    )
+    with open(Log_File_name, 'w')as file:
+        json.dump(file_data, file, indent=4)
 
 def detect():
     # Maintain a log of previous values to
@@ -24,48 +50,40 @@ def detect():
     history = []
     beats = []
     beat = False
-    pipe = mrsm.Pipe("plugin:bio_metric", "bpm", "raw", instance="sql:local", columns={"datetime":"UTCDateTime"})
-    counter = 0
+    beat_counter = 0
+    threshold_modifier = 0.05
+    record_threshold = 30
     while True:
+        # read and append values to a list that stores the analog signal strength
         v = mcp.read_adc(0)
-
         history.append(v)
-
-        # Get the tail, up to MAX_HISTORY length
         history = history[-MAX_HISTORY:]
 
+        # set threshholds on what to count for a new beat logic
         minima, maxima = min(history), max(history)
-        threshold_on = maxima - (maxima - minima) * 0.05    # 3/4
-        threshold_off = minima + (maxima - minima) * 0.05      # 1/2
+        threshold_on = maxima - (maxima - minima) * threshold_modifier
+        threshold_off = minima + (maxima - minima) * threshold_modifier
 
-#        print(f"\nminima: {minima} maxima: {maxima} on: {threshold_on} off: {threshold_off}")
-
+        # count a new beat if the value detected is over the beat threshold
+        # and if the beat toggle is false
         if v > threshold_on and beat == False:
+            beat_counter +=  1
             beat = True
             beat_time = time.time()
             beats.append(beat_time)
-            last_beat = beat_time
             beats = beats[-TOTAL_BEATS:]
-            trigger_print = counter % 30 == 0
-            calculate_bpm(beats, trigger_print, pipe)
-            counter +=  1
+            # every set quantity of beats record to the json
+            if beat_counter % record_threshold == 0:
+                calculate_bpm(beats)
+                beat_counter = 0
 
-#            print(f"\nminima: {minima} maxima: {maxima} on: {threshold_on} off: {threshold_off}")
- #           print(v)
-
-
-  #          print(f"beat: {counter}")
-
-
+        # If the beat toggle is on, the value is bellow the beat threshold
+        # and there is time since the last beat.
         if v < threshold_off and beat == True and time.time() - beat_time  > 0.01:
-
-#            print(f"\nminima: {minima} maxima: {maxima} on: {threshold_on} off: {threshold_off}")
-   #         print(v)
-
             beat = False
 
 
-SPI_PORT   = 0
+SPI_PORT = 0
 SPI_DEVICE = 0
 mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 
@@ -76,6 +94,17 @@ MAX_HISTORY = 1000
 history = []
 beat = False
 beats = 0
+
+# set up logging file
+date = datetime.datetime.utcnow().strptime("%d:%m:%Y")
+Log_File_name = f".data_logs/temperature_data.json" + date
+with open(Log_File_name, 'r+')as file:
+    file_data = json.load(file)
+    if "readings" not in file_data:
+        file_data = {
+            "readings" : []
+        }
+        json.dump(file_data, file, indent=4)
 
 TOTAL_BEATS = 30
 detect()
--- temperature.py
+++ temperature.py
@@ -2,8 +2,8 @@
 import os
 import glob
 import time
-import meerschaum as mrsm
 import datetime
+import json
  
 # Initialize the GPIO Pins
 os.system('modprobe w1-gpio')  # Turns on the GPIO module
@@ -13,40 +13,77 @@ os.system('modprobe w1-therm') # Turns on the Temperature module
 base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
+
+# maintains one json file per day to avoid data bloating
+# if unable to sync to database
+def update_file_name():
+    global date
+    date = datetime.datetime.utcnow().strptime("%d:%m:%Y")
+    global Log_File_name
+    Log_File_name = ".data_logs/heart_rate_data.json" + date
+    global file_data
+    file_data.clear()
+    file_data = {
+      "readings" : []
+    }
  
 # A function that reads the sensors data
 def read_temp_raw():
-  f = open(device_file, 'r') # Opens the temperature device file
-  lines = f.readlines() # Returns the text
-  f.close()
-  return lines
+    f = open(device_file, 'r') # Opens the temperature device file
+    lines = f.readlines() # Returns the text
+    f.close()
+    return lines
  
 # Convert the value of the sensor into a temperature
 def read_temp():
-  lines = read_temp_raw() # Read the temperature 'device file'
+    lines = read_temp_raw() # Read the temperature 'device file'
+    
+    # While the first line does not contain 'YES', wait for 0.2s
+    # and then read the device file again.
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = read_temp_raw()
  
-  # While the first line does not contain 'YES', wait for 0.2s
-  # and then read the device file again.
-  while lines[0].strip()[-3:] != 'YES':
-    time.sleep(0.2)
-    lines = read_temp_raw()
+    # Look for the position of the '=' in the second line of the
+    # device file.
+    equals_pos = lines[1].find('t=')
  
-  # Look for the position of the '=' in the second line of the
-  # device file.
-  equals_pos = lines[1].find('t=')
- 
-  # If the '=' is found, convert the rest of the line after the
-  # '=' into degrees Celsius, then degrees Fahrenheit
-  if equals_pos != -1:
-    temp_string = lines[1][equals_pos+2:]
-    temp_c = float(temp_string) / 1000.0
-    temp_f = temp_c * 9.0 / 5.0 + 32.0
-    return temp_c, temp_f
- 
-# Print out the temperature until the program is stopped.
-pipe = mrsm.Pipe("plugin:bio_metric", "temp", "raw", instance="sql:local", columns={"datetime":"UTCDateTime"})
-while True:
-  cel, far = read_temp()
-#  print(f"temp cel: {cel}, temp far: {far}")
-  pipe.sync({"UTCDateTime":[datetime.datetime.utcnow()],"celsius":[cel], "farenheit":[far]})
-  time.sleep(30)
+    # If the '=' is found, convert the rest of the line after the
+    # '=' into degrees Celsius, then degrees Fahrenheit
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
+        temp_c = float(temp_string) / 1000.0
+        temp_f = temp_c * 9.0 / 5.0 + 32.0
+        return temp_c, temp_f
+
+def detect():
+    global file_data
+    while True:
+        cel, far = read_temp()
+        record_time = datetime.datetime.utcnow()
+        if record_time.strptime("%d:%m:%Y") != date:
+            update_file_name()
+        file_data["readings"].append(
+            {
+                "UTCDatetime": record_time,
+                "fahrenheit": far,
+                "celsius": cel
+            }
+        )
+        with open(Log_File_name, 'w')as file:
+            json.dump(file_data, file, indent=4)
+        time.sleep(30)
+
+# set file to save values to
+date = datetime.datetime.utcnow().strptime("%d:%m:%Y")
+Log_File_name = f".data_logs/heart_rate_data.json" + date
+# set up logging file
+with open(Log_File_name, 'r+')as file:
+    file_data = json.load(file)
+    if "readings" not in file_data:
+        file_data = {
+            "readings" : []
+        }
+        json.dump(file_data, file, indent=4)
+
+detect()
